5902675d969c6a78f8dd6161e785fe9b42e5d119
Ahmad Musa <ahmadmusa0987@gmail.com>
2025-03-17 17:35:48 +0600

    setting express

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get('',(req,res) => {
+    res.send("Hello");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`);
+});
