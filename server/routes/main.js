84111ea0f161e5c8cc68f7843a3474ca97b02c4d
Ahmad Musa <ahmadmusa0987@gmail.com>
2025-03-20 15:41:07 +0600

    DB: data added statically

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router =  express.Router();
 
-// Routes
-router.get('',(req, res) => {
+const Post = require('../models/Post');
+
+// GET 
+// HOME route
+router.get('', (req, res) => {
 
     const locals = {
         title: "NodeJS Blogger",
@@ -13,8 +16,52 @@ router.get('',(req, res) => {
     res.render('index', { locals });
 });
 
+
 router.get('/about',(req, res) => {
     res.render('about');
 });
+
+
+
+function insertPostData() {
+    Post.insertMany([
+        {
+            title: "Building a Blog",
+            body: "This is the body"
+        },        
+        {
+            title: "Go with a Blog",
+            body: "This is the body"
+        },
+        {
+            title: "Life of a Blogger",
+            body: "This is the body"
+        },
+        {
+            title: "Blogging: My daily works",
+            body: "This is the body"
+        },
+        {
+            title: "Don't make mistake in your Blog",
+            body: "This is the body"
+        },
+        {
+            title: "Lifestyle: Blog",
+            body: "This is the body"
+        },
+        {
+            title: "Journalism with Blog",
+            body: "This is the body"
+        },
+        {
+            title: "I want to leave Blog",
+            body: "This is the body"
+        },
+    ])
+}
+
+insertPostData();
+
+
 
 module.exports = router;
