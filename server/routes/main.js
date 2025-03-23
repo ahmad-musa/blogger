55f8e7ec36fdf2a1c07b394e284afaa9bc688d17
Ahmad Musa <ahmadmusa0987@gmail.com>
2025-03-23 17:55:59 +0600

    Route: Search ok

--- server/routes/main.js
+++ server/routes/main.js
@@ -66,6 +66,43 @@ router.get('', async (req, res) => {
 
 
 // GET 
+// Search :id route
+router.post('/search', async (req, res) => {
+
+    try {
+        
+        const locals = {
+            title: "Search",
+            description: "Simple blog created with NodeJS, Express & MongoDB."
+        }
+        
+        let searchTerm = req.body.searchTerm;
+        // console.log(searchTerm);
+        
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "")
+        
+        const data = await Post.find({
+            $or: [
+                { title : {$regex: new RegExp(searchNoSpecialChar, 'i')}},
+                { body : {$regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
+
+        res.render('search', { 
+            data,
+            locals 
+        });
+        // res.send(searchTerm);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+
+// GET 
 // Post :id route
 router.get('/post/:id', async (req, res) => {
 
@@ -91,8 +128,6 @@ router.get('/post/:id', async (req, res) => {
     }
 
 });
-
-
 
 
 
